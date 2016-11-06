8ada1e2a370857b817975f078b3824284ab0ec21
Ubuntu <ubuntu@ip-172-31-41-248.us-west-2.compute.internal>
2016-11-06 06:19:04 +0000

    startall script needs abs path to manage.py

--- tools/startall.sh
+++ tools/startall.sh
@@ -8,5 +8,5 @@ export PYTHONPATH=/home/ubuntu/expooh/repo/adwise-userprofile-mgr/
 source /home/ubuntu/expooh/venv/bin/activate
 screen -d -m celery --app=atlas_ws.celery_config:app worker --loglevel=INFO --concurrency=2 -n worker1.%h
 echo "Starting app server..."
-screen -d -m python manage.py runserver 172.31.41.248:8000
+screen -d -m python /home/ubuntu/expooh/repo/adwise-userprofile-mgr/manage.py runserver 172.31.41.248:8000
 
