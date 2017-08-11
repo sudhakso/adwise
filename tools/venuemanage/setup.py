823d1c246bddb3d7fd2e1dc6be5eeccf6105ce5a
sonu <sonu.sudhakaran@gmail.com>
2017-08-11 08:44:46 -0700

    Adding venuemanage utility project

---
+++ tools/venuemanage/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='adwise-venuemanage-util',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['venuemanage']                  # The name of your scipt, and also the command you'll be using for calling it
+)
