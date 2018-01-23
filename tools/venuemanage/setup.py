980d392abd987b1716b4a1cd2c785266ef055fb2
Ubuntu <ubuntu@ip-172-31-19-119.us-east-2.compute.internal>
2018-01-23 17:57:28 +0000

    update utility

--- tools/venuemanage/setup.py
+++ tools/venuemanage/setup.py
@@ -2,5 +2,9 @@ from setuptools import setup
 setup(
     name='adwise-venuemanage-util',    # This is the name of your PyPI-package.
     version='0.2',                          # Update the version number for new releases
-    scripts=['venuemanage']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['venuemanage'],                  # The name of your scipt, and also the command you'll be using for calling it
+    install_requires=[
+          'requests',
+          'pika'
+      ]
 )
